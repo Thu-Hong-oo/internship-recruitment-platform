f82630df2aa998a37e1d238cb52bfd8f4317c6bf
nguyenngoctuongvan3003 <nguyenngoctuongvan3003@gmail.com>
2025-12-11 09:37:49 +0700

    add application stats for dashboard

--- backend/src/routes/applications.js
+++ backend/src/routes/applications.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { protect, authorize } = require('../middleware/auth');
-const { requireVerifiedEmployer } = require('../middleware/employerVerification');
+const {
+  requireVerifiedEmployer,
+} = require('../middleware/employerVerification');
 const {
   updateApplicationStatus,
   getEmployerApplications,
@@ -31,4 +33,3 @@ router.get(
 );
 
 module.exports = router;
-
